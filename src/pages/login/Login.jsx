8ef5e05966b9a5bcf6f41d13ad1b647240e42edb
hamim <hamim2114@gmail.com>
2025-01-17 15:15:07 +0600

    updated

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -189,7 +189,7 @@ const Login = (props) => {
                   width: { xs: '70%', md: '200px' },
                   mb: 2
                 }}>
-                  <img width='100%' src="/Logo.svg" alt="" />
+                  <img width='100%' src="/logo.gif" alt="" />
                 </Box>
               </Stack>
               <Typography sx={{ fontWeight: 600, fontSize: '25px', mb: 3, textAlign: 'center' }}>Leverandørinnlogging</Typography>
